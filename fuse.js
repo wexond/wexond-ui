d01cf2f5c420291f99b8e10b0091eb13743637a9
Mikoaj Palkiewicz <xnerhu@gmail.com>
2019-06-13 00:10:56 +0200

    Update fuse config

--- fuse.js
+++ fuse.js
@@ -122,7 +122,7 @@ Sparky.task('default', async () => {
 Sparky.task('lib', async () => {
   await new Builder({
     name: 'index',
-    instructions: '> src/index.ts',
+    instructions: '> [src/index.ts]',
     plugins: [
       CopyPlugin({
         files: ['*.woff2', '*.svg'],
