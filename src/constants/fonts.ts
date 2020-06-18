0bb0030e186c94c204439aa00fbf15522d9348b8
Mikołaj Palkiewicz <xnerhu@gmail.com>
2020-06-18 19:18:49 +0200

    feat: add fonts (#6)

---
+++ src/constants/fonts.ts
@@ -0,0 +1,3 @@
+export const FONT_ROBOTO_REGULAR = require('~/resources/fonts/roboto-regular.woff2');
+export const FONT_ROBOTO_MEDIUM = require('~/resources/fonts/roboto-medium.woff2');
+export const FONT_ROBOTO_LIGHT = require('~/resources/fonts/roboto-light.woff2');
