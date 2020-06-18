4209d181a7256a058bd40a0c8d19a5025b7b0a64
Mikołaj Palkiewicz <xnerhu@gmail.com>
2020-06-18 19:46:48 +0200

    feat: add global styles (#7)
    * feat: storybook decorator
    
    * feat: add ui style

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-// export * from './components';
-export * from './mixins';
-export * from './constants';
--- stories/test.stories.tsx
+++ stories/test.stories.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
-
-import { Button } from '../src/components/Button';
 import { withInfo } from '@storybook/addon-info';
 
+import { Button } from '../src/components/Button';
+import { decorate } from './utils/storybook';
+
 const stories = storiesOf('Components', module);
+
+decorate(stories);
 
 stories.add(
   'Button',
