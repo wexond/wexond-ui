f62a5cf2491acd7d5e018c52bcaf26447d7dc966
xnerhu <xnerhu@gmail.com>
2020-07-06 19:28:35 +0200

    chore: config clean up

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,28 @@
+const { resolve } = require('path');
+
+const INCLUDE = resolve('src');
+
 module.exports = {
-  stories: ['../src/stories/**/*.stories.(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.story.(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-knobs', '@storybook/addon-actions'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.tsx|ts$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+          options: {
+            transpileOnly: true,
+          },
+        },
+        { loader: 'react-docgen-typescript-loader' },
+      ],
+    });
+
+    config.resolve.modules.push('node_modules');
+    config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias['~'] = INCLUDE;
+
+    return config;
+  },
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
-const { getConfig, dev } = require('./webpack.base');
+const { resolve } = require('path');
+const createStyledComponentsTransformer = require('typescript-plugin-styled-components')
+  .default;
 
-module.exports = getConfig({
+const dev = process.env.NODE_ENV === 'development';
+const INCLUDE = resolve(__dirname, 'src');
+
+const styledComponentsTransformer = createStyledComponentsTransformer({
+  minify: !dev,
+  displayName: true,
+});
+
+module.exports = {
   mode: dev ? 'development' : 'production',
 
   entry: './src/index.ts',
+  devtool: 'source-map',
 
   output: {
     filename: 'index.js',
@@ -12,11 +23,49 @@ module.exports = getConfig({
     library: 'wexond-ui',
   },
 
-  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(png|gif|jpg|woff2|ttf|svg)$/,
+        include: INCLUDE,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx|ts$/,
+        include: INCLUDE,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: dev,
+              getCustomTransformers: () => ({
+                before: [styledComponentsTransformer],
+              }),
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    alias: {
+      '~': INCLUDE,
+    },
+  },
 
   externals: {
     react: 'react',
     'react-dom': 'react-dom',
     'styled-components': 'styled-components',
   },
-});
+};
