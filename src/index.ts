1b4675b7b7c329b7ae4c73af7ae8482f2c610e9e
Mikołaj Palkiewicz <xnerhu@gmail.com>
2021-07-03 21:39:50 +0200

    feat: add slider (#30)

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export * from './components/Droppable';
 export * from './components/Draggable';
 export * from './components/Skeleton';
 export * from './components/Query';
+export * from './components/Slider';
 
 export * from './popup/popup';
 
