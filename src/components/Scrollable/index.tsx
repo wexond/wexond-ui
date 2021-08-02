12c90f59afeb3087ee35f9cf856d818d5accad9e
xnerhu <xnerhu@gmail.com>
2021-08-02 15:17:17 +0200

    refactor: scrollable

--- src/components/Scrollable/index.tsx
+++ src/components/Scrollable/index.tsx
@@ -1,44 +1,193 @@
 import React from 'react';
 
 import { ComponentProps } from '../../core/component';
+import { minmax } from '../../utils/math';
 import { mergeEvents, mergeRefs } from '../../utils/merge';
-import { StyledScrollable } from './style';
+import {
+  StyledScrollable,
+  ScrollTrack,
+  ScrollThumb,
+  Container,
+  SCROLLBAR_THUMB_MARGIN,
+} from './style';
 
 export interface ScrollableProps
   extends React.HTMLAttributes<HTMLDivElement>,
     ComponentProps {}
 
+const getScrollRatio = (viewportSize: number, contentSize: number) => {
+  return viewportSize / contentSize;
+};
+
+const getThumbPos = (
+  scrollPos: number,
+  viewportSize: number,
+  thumbSize: number,
+  scrollRatio: number,
+) => {
+  return minmax(scrollPos * scrollRatio, 0, viewportSize);
+};
+
+const getScrollJump = (
+  viewportSize: number,
+  contentSize: number,
+  thumbSize: number,
+) => {
+  const scrollTrackSpace = contentSize - viewportSize;
+  const scrollThumbSpace = viewportSize - thumbSize; // - SCROLLBAR_THUMB_MARGIN * 4;
+
+  return Math.round(scrollTrackSpace / scrollThumbSpace);
+};
+
+const getScrollPosition = (
+  scrollJump: number,
+  startPos: number,
+  delta: number,
+  contentSize: number,
+) => {
+  return minmax(startPos + scrollJump * delta, 0, contentSize);
+};
+
+const getThumbSize = (
+  viewportSize: number,
+  contentSize: number,
+  trackWidth: number,
+  trackOffset: number,
+) => {
+  const viewableRatio = viewportSize / contentSize;
+  const scrollBarArea = trackWidth; // - SCROLLBAR_THUMB_MARGIN * 2;
+  console.log(scrollBarArea);
+  return Math.round(scrollBarArea * viewableRatio);
+};
+
+const getContentSize = (viewportSize: number, scrollSize: number) => {
+  return scrollSize - viewportSize;
+};
+
 export const Scrollable = React.forwardRef<HTMLDivElement, ScrollableProps>(
-  ({ as, onWheel, onKeyDown, ...props }, ref) => {
-    const _ref = React.useRef<HTMLDivElement | null>(null);
+  ({ children, onScroll, ...props }, ref) => {
+    const containerRef = React.useRef<HTMLDivElement | null>(null);
+    const trackRef = React.useRef<HTMLDivElement | null>(null);
+    const thumbRef = React.useRef<HTMLDivElement | null>(null);
 
-    const _onWheel = React.useCallback((e: React.WheelEvent) => {
-      if (!_ref.current) return;
+    const startThumbPos = React.useRef<number>(0);
+    const startScrollTop = React.useRef<number>(0);
 
-      _ref.current.scrollTop = _ref.current.scrollTop + e.deltaY;
+    const mutationObserver = React.useRef<MutationObserver | null>(null);
+
+    const _onScroll = React.useCallback(() => {
+      if (!containerRef.current || !thumbRef.current) return;
+
+      const { clientWidth, scrollWidth, scrollLeft } = containerRef.current;
+
+      const contentSize = getContentSize(clientWidth, scrollWidth);
+      const scrollRatio = getScrollRatio(clientWidth, contentSize);
+      const thumbPos = getThumbPos(
+        scrollLeft,
+        clientWidth,
+        thumbRef.current.clientWidth,
+        scrollRatio,
+      );
+
+      console.log(scrollLeft, scrollRatio);
+
+      thumbRef.current.style.transform = `translateX(${thumbPos}px)`;
+
+      console.log(`scroll: ${scrollLeft}/${contentSize}`);
     }, []);
 
-    const _onKeyDown = React.useCallback((e: React.KeyboardEvent) => {
-      if (!_ref.current) return;
+    const onWindowMouseMove = React.useCallback((e: MouseEvent) => {
+      if (!containerRef.current || !thumbRef.current) return;
 
-      if (e.key === 'Home') {
-        _ref.current.scrollTop = 0;
-      } else if (e.key === 'End') {
-        _ref.current.scrollTop = _ref.current.scrollHeight;
+      const delta = e.pageX - startThumbPos.current;
+
+      const { clientWidth, scrollWidth } = containerRef.current;
+      const contentSize = getContentSize(clientWidth, scrollWidth);
+
+      const scrollJump = getScrollJump(
+        contentSize,
+        scrollWidth,
+        thumbRef.current.clientWidth,
+      );
+
+      const scrollPos = getScrollPosition(
+        scrollJump,
+        startScrollTop.current,
+        delta,
+        scrollWidth,
+      );
+
+      containerRef.current.scrollLeft = scrollPos;
+    }, []);
+
+    const onWindowMouseUp = React.useCallback(() => {
+      window.removeEventListener('mousemove', onWindowMouseMove);
+      window.removeEventListener('mouseup', onWindowMouseUp);
+    }, [onWindowMouseMove]);
+
+    const onThumbMouseDown = React.useCallback(
+      (e: React.MouseEvent) => {
+        if (!containerRef.current) return;
+
+        startThumbPos.current = e.pageX;
+        startScrollTop.current = containerRef.current.scrollLeft;
+
+        window.addEventListener('mousemove', onWindowMouseMove);
+        window.addEventListener('mouseup', onWindowMouseUp);
+      },
+      [onWindowMouseMove, onWindowMouseUp],
+    );
+
+    const updateThumbSize = React.useCallback(() => {
+      if (!thumbRef.current || !containerRef.current || !trackRef.current)
+        return;
+
+      const { scrollWidth, clientWidth } = containerRef.current;
+      const contentSize = getContentSize(clientWidth, scrollWidth);
+
+      const thumbSize = getThumbSize(
+        clientWidth,
+        scrollWidth,
+        trackRef.current.clientWidth,
+        trackRef.current.offsetLeft,
+      );
+
+      console.log(thumbSize, trackRef.current.clientWidth);
+
+      thumbRef.current.style.width = thumbSize + 'px';
+    }, []);
+
+    React.useEffect(() => {
+      if (containerRef.current) {
+        const observer = new MutationObserver(() => {
+          updateThumbSize();
+        });
+
+        observer.observe(containerRef.current, {
+          attributes: true,
+          childList: true,
+          subtree: true,
+        });
       }
-    }, []);
 
-    const Root = as || StyledScrollable;
+      setTimeout(() => {
+        updateThumbSize();
+      }, 1);
+    }, [updateThumbSize]);
 
     return (
-      <Root
-        ref={mergeRefs(ref, _ref)}
-        {...mergeEvents({
-          onWheel: [onWheel, _onWheel],
-          onKeyDown: [onKeyDown, _onKeyDown],
-        })}
-        {...props}
-      />
+      <StyledScrollable>
+        <Container
+          ref={mergeRefs(ref, containerRef)}
+          {...mergeEvents({ onScroll: [_onScroll, onScroll] })}
+          {...props}
+        >
+          {children}
+        </Container>
+        <ScrollTrack ref={trackRef}>
+          <ScrollThumb ref={thumbRef} onMouseDown={onThumbMouseDown} />
+        </ScrollTrack>
+      </StyledScrollable>
     );
   },
 );
--- src/components/Scrollable/style.ts
+++ src/components/Scrollable/style.ts
@@ -1,3 +1,53 @@
 import styled from 'styled-components';
 
-export const StyledScrollable = styled.div``;
+import { centerHorizontal, centerVertical } from '../../mixins/positioning';
+import { noUserSelect } from '../../mixins/user-selection';
+
+export const SCROLLBAR_THUMB_MARGIN = 3;
+
+export const StyledScrollable = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+`;
+
+export const Container = styled.div`
+  overflow: hidden;
+  ${noUserSelect};
+`;
+
+export const ScrollThumb = styled.div`
+  height: 1px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 16px;
+  overflow: hidden;
+  will-change: transform;
+  transition: 0.1s background-color, 0.1s width, 0.1s height;
+
+  &:active {
+    background-color: red;
+  }
+`;
+
+export const ScrollTrack = styled.div`
+  height: 12px;
+  width: 100%;
+  /* width: calc(100% - ${SCROLLBAR_THUMB_MARGIN * 2}px); */
+  /* margin: ${SCROLLBAR_THUMB_MARGIN}px; */
+  /* padding: 0px ${SCROLLBAR_THUMB_MARGIN}px; */
+  flex-shrink: 0;
+  position: relative;
+  border-radius: 8px;
+  display: flex;
+  overflow: hidden;
+  align-items: center;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.24);
+  }
+
+  &:hover > ${ScrollThumb} {
+    height: 6px;
+  }
+`;
